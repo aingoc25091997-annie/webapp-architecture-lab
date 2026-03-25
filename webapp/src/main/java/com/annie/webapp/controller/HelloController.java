bd57b676ac8e943c5741771ab20f15c065c8983d
annie <aingoc25091997@gmail.com>
2026-03-25 21:46:33 +0700

    hello-world

--- webapp/src/main/java/com/annie/webapp/controller/HelloController.java
+++ webapp/src/main/java/com/annie/webapp/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.annie.webapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
 
